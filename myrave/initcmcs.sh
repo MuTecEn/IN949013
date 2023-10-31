d18bf5831a531b5a6260491de7615a90831b0669
Anna-Maria Christodoulou <annammc@ml1.hpc.uio.no>
2023-10-31 22:12:27 +0100

    cuda gpu

--- myrave/initcmcs.sh
+++ myrave/initcmcs.sh
@@ -2,7 +2,7 @@
 module load FFmpeg/4.3.2-GCCcore-10.3.0
 
 module load Python/3.9.6-GCCcore-11.2.0
-python -m venv ./raveenv
+# python -m venv ./raveenv
 source ./raveenv/bin/activate
 
 nohup python -u ravev2_training.py run --username annammc > /logs/ravev2_training.log 2>&1 & echo $! > annammc.pid
--- myrave/rave_test.sh
+++ myrave/rave_test.sh
@@ -1,6 +1,6 @@
 rave preprocess --input_path /scratch/users/annammc/maestro/maestro-v3.0.0/2004 --output_path /scratch/users/annammc/output
 
-rave train --config v2 --db_path /scratch/users/annammc/output/data.mdb --name maestro
+rave train --config v2 --db_path /scratch/users/annammc/output/ --name maestro
 
 # rave export --run /path/to/your/run (--streaming)
 
