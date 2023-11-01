fc8a9ec1cf8ff47336684917b8c469c8f25a58f1
Anna-Maria Christodoulou <annammc@ml1.hpc.uio.no>
2023-11-01 18:36:05 +0100

    export

--- myrave/rave_test.sh
+++ myrave/rave_test.sh
@@ -1,5 +1,5 @@
 rave preprocess --input_path /scratch/users/annammc/maestro/maestro-v3.0.0/2004 --output_path /scratch/users/annammc/output
-
+export CUDA_VISIBLE_DEVICES=4
 rave train --config v2 --db_path /scratch/users/annammc/output/ --name maestro
 
 # rave export --run /path/to/your/run (--streaming)
